207fa1bbe68ccbc59b540655a71cd65cf9e3ee80
saehwan kim <saehwan1013@gmail.com>
2021-06-23 13:51:15 -0700

    added pulling random number from raffle formula

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,27 +9,49 @@ namespace ConsoleUI
 {
     class Program
     {
-        static string GetUserInput()
+        static string GetUserInput(string input)
         {
-            Console.WriteLine("Please input guest's name.");
-            return  string GetUserInput = Console.ReadLine();
+            
+            Console.WriteLine(input);
+            return GuestName = Console.ReadLine();
         }
+
         static void GetUserInfo()
         {
             string otherName;
             do 
             {
-                GetUserInput();
+                string name = GetUserInput("Please enter a name");
+                raffleNumber = GenerateRandomeNumber(min, max);
                 Console.WriteLine("Do you want to add another name?");
                 string otherName1 = Console.ReadLine();
                 otherName = otherName1.ToLower();
-
+                AddGuestsInRaffle(raffleNumber, GuestName );
             }
-            while (otherName == 'yes');
+            while (otherName == "yes");
         }
+
         static int GenerateRandomeNumber(int min, int max)
         {
             return _rdm.Next(min, max);
+        }
+
+        static void AddGuestsInRaffle(int raffleNumber, string GuestName)
+        {
+            guests.Add(raffleNumber, GuestName);
+        }
+
+        static void PrintGuestsName()
+        {
+            int randomNumber;
+            do
+            {
+                randomNumber = GenerateRandomeNumber(min, max);
+            }
+            while (guests.ContainsKey(randomNumber));
+            
+            
+            
         }
 
 
@@ -37,7 +59,7 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
 
-           
+            GetUserInfo();
 
 
         }
@@ -48,58 +70,58 @@ namespace ConsoleUI
         private static int max = 9999;
         private static int raffleNumber;
         private static Random _rdm = new Random();
+        private static string GuestName;
 
 
 
 
 
+        //static void MultiLineAnimation() // Credit: https://www.michalbialecki.com/2018/05/25/how-to-make-you-console-app-look-cool/
+        //{
+        //    var counter = 0;
+        //    for (int i = 0; i < 30; i++)
+        //    {
+        //        Console.Clear();
 
-        static void MultiLineAnimation() // Credit: https://www.michalbialecki.com/2018/05/25/how-to-make-you-console-app-look-cool/
-        {
-            var counter = 0;
-            for (int i = 0; i < 30; i++)
-            {
-                Console.Clear();
+        //        switch (counter % 4)
+        //        {
+        //            case 0:
+        //                {
+        //                    Console.WriteLine("         ╔════╤╤╤╤════╗");
+        //                    Console.WriteLine("         ║    │││ \   ║");
+        //                    Console.WriteLine("         ║    │││  O  ║");
+        //                    Console.WriteLine("         ║    OOO     ║");
+        //                    break;
+        //                };
+        //            case 1:
+        //                {
+        //                    Console.WriteLine("         ╔════╤╤╤╤════╗");
+        //                    Console.WriteLine("         ║    ││││    ║");
+        //                    Console.WriteLine("         ║    ││││    ║");
+        //                    Console.WriteLine("         ║    OOOO    ║");
+        //                    break;
+        //                };
+        //            case 2:
+        //                {
+        //                    Console.WriteLine("         ╔════╤╤╤╤════╗");
+        //                    Console.WriteLine("         ║   / │││    ║");
+        //                    Console.WriteLine("         ║  O  │││    ║");
+        //                    Console.WriteLine("         ║     OOO    ║");
+        //                    break;
+        //                };
+        //            case 3:
+        //                {
+        //                    Console.WriteLine("         ╔════╤╤╤╤════╗");
+        //                    Console.WriteLine("         ║    ││││    ║");
+        //                    Console.WriteLine("         ║    ││││    ║");
+        //                    Console.WriteLine("         ║    OOOO    ║");
+        //                    break;
+        //                };
+        //        }
 
-                switch (counter % 4)
-                {
-                    case 0:
-                        {
-                            Console.WriteLine("         ╔════╤╤╤╤════╗");
-                            Console.WriteLine("         ║    │││ \   ║");
-                            Console.WriteLine("         ║    │││  O  ║");
-                            Console.WriteLine("         ║    OOO     ║");
-                            break;
-                        };
-                    case 1:
-                        {
-                            Console.WriteLine("         ╔════╤╤╤╤════╗");
-                            Console.WriteLine("         ║    ││││    ║");
-                            Console.WriteLine("         ║    ││││    ║");
-                            Console.WriteLine("         ║    OOOO    ║");
-                            break;
-                        };
-                    case 2:
-                        {
-                            Console.WriteLine("         ╔════╤╤╤╤════╗");
-                            Console.WriteLine("         ║   / │││    ║");
-                            Console.WriteLine("         ║  O  │││    ║");
-                            Console.WriteLine("         ║     OOO    ║");
-                            break;
-                        };
-                    case 3:
-                        {
-                            Console.WriteLine("         ╔════╤╤╤╤════╗");
-                            Console.WriteLine("         ║    ││││    ║");
-                            Console.WriteLine("         ║    ││││    ║");
-                            Console.WriteLine("         ║    OOOO    ║");
-                            break;
-                        };
-                }
-
-                counter++;
-                Thread.Sleep(200);
-            }
-        }
+        //        counter++;
+        //        Thread.Sleep(200);
+        //    }
+        //}
     }
 }
